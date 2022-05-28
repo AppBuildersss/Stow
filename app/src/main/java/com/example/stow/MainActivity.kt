d9227e465d0524d434808e515ee466a19ee32d42
“ShawnYip-Au” <shawnyipau@gmail.com>
2022-05-28 19:09:00 +0800

    run python script

--- app/src/main/java/com/example/stow/MainActivity.kt
+++ app/src/main/java/com/example/stow/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.stow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stow.databinding.ActivityMainBinding
+
+import com.chaquo.python.PyException;
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initPython()
+        binding.pythonTextview.text = getPythonScript()
     }
+
+    private fun initPython() {
+        if (!Python.isStarted()){
+            Python.start(AndroidPlatform(this))
+        }
+    }
+
+    private fun getPythonScript(): String {
+        val python = Python.getInstance()
+        val pythonFile = python.getModule("script")
+        return pythonFile.callAttr("run").toString()
+    }
+
 }
---
+++ app/src/main/python/script.py
@@ -0,0 +1,29 @@
+from pytrends.request import TrendReq
+
+def run():
+    return get_stats()
+
+def get_stats():
+    pytrends = TrendReq(hl='en-US', tz=360)
+    kw_list = ["Ted Lasso Season 2", "nike travis scott", "monkeypox", "ukraine"]
+    keywords = []
+
+    s = "ranking" + "\n"
+
+    def check():
+        pytrends.build_payload(kw_list, cat=0, timeframe='now 7-d', geo='', gprop='')
+        data = pytrends.interest_over_time()
+        mean = round(data.mean(), 2)
+        #print(kw + ': ' + str(mean[kw]))
+        #s = s + kw + ': ' + str(mean[kw]) + '\n'i
+        return str(mean[kw]) + "\n"
+
+
+    for kw in kw_list:
+        keywords.append(kw)
+        s = s + check()
+        keywords.pop()
+
+    return s
+
+print(run())
