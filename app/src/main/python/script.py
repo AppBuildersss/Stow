8ff2c49c5a94fcd812ec2329f327e41170ea6009
“ShawnYip-Au” <shawnyipau@gmail.com>
2022-06-26 16:17:38 +0800

    UI skeleton

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -6,20 +6,25 @@ def getResult(word_01, word_02, word_03, word_04, word_05):
     kwList = [word_01, word_02, word_03, word_04, word_05]
     keywords = []
 
-    s = "Trend %" + "\n"
+    s = ""
 
     def check():
         pytrends.build_payload(kwList, cat=0, timeframe='now 7-d', geo='', gprop='')
         data = pytrends.interest_over_time()
         mean = round(data.mean(), 2)
-        return str(mean[kw]) + "\n"
+        #return str(mean[kw]) + "\n"
+        return str(mean[kw])
 
     kwList = [x for x in kwList if x != ""]
 
 
     for kw in kwList:
         keywords.append(kw)
-        s = s + kw + " " + check()
+        #s = s + kw + " " + check()
+        #s = s + check() + " " + kw + "\n"
+        s = s + check() + "\n"
         keywords.pop()
 
     return s
+
+
