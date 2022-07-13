8a90fc03bb3d9e7e66b36d8bef001da357c40644
“ShawnYip-Au” <shawnyipau@gmail.com>
2022-07-13 12:34:19 +0800

    sign ooyt button

--- app/src/main/java/com/example/stow/LoggedInMainActivity.kt
+++ app/src/main/java/com/example/stow/LoggedInMainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.stow
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,16 +9,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import androidx.navigation.fragment.findNavController
 import com.example.stow.databinding.LoggedInActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoggedInMainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: LoggedInActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Entry point to access Firebase Database
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding = LoggedInActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -41,7 +45,16 @@ class LoggedInMainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_sign_out -> {
+                //sign out
+                firebaseAuth.signOut()
+
+                //return to FirstFragment
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
