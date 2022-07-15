053ee3945eccd73c1dda726afc64f4b47e991e85
“ShawnYip-Au” <shawnyipau@gmail.com>
2022-07-15 18:08:22 +0800

    Hide keyboard when logging in

--- app/src/main/java/com/example/stow/FirstFragment.kt
+++ app/src/main/java/com/example/stow/FirstFragment.kt
@@ -1,16 +1,18 @@
 package com.example.stow
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.stow.databinding.FragmentFirstBinding
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.delay
+
 
 class FirstFragment : Fragment() {
 
@@ -56,6 +58,7 @@ class FirstFragment : Fragment() {
         if (email.isNotEmpty() && password.isNotEmpty()) {
             firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
                 if (it.isSuccessful) {
+                    context?.let { it1 -> view?.let { it2 -> hideKeyboard(it1, it2)} };
                     val intent = Intent(context, LoggedInMainActivity::class.java)
                     startActivity(intent)
                     Runtime.getRuntime().exit(0)
@@ -66,5 +69,11 @@ class FirstFragment : Fragment() {
         } else {
             Toast.makeText(context, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun hideKeyboard(context: Context, view: View) {
+        val imm: InputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
